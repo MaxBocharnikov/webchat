29726c4cb3169400829aa3703ebf32d9af443f32
Max Bocharnikov <bocharnikov3009@gmail.com>
2022-01-30 17:58:37 +0700

    some basic logic with web sockets

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./styles/index.scss";
 
-const App = () => {
+const socket = new WebSocket("ws://localhost:5000/");
 
+let userName;
+let id;
+
+socket.onopen = () => {
+    userName = prompt('Как к вам обращаться, сэр?');
+    id = Date.now() + 'id';
+};
+
+socket.onClose = () => {
+    alert('Подключение потеряно');
+};
+
+const App = () => {
+    const [text, setText] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    socket.onmessage = event => {
+        const message = JSON.parse(event.data);
+        setMessages([...messages, message]);
+    }
+
+    const onSend = event => {
+        event.preventDefault();
+        const payload = {
+            id,
+            userName,
+            message: text,
+            date: Date.now(),
+        }
+        socket.send(JSON.stringify({
+            event: 'chat-message',
+            payload
+        }));
+        setText('');
+    }
+
+    console.log(messages)
     return (
-        <h1>Hello World!!!</h1>
+        <div className='wrapper'>
+            <h1 className='title'>Это чат</h1>
+            <div className='chatWindow'>
+                <div className="messages">
+                    {messages.map(m => (
+                        <div key={m.date} className={`message ${m.id === id ? 'own-message' : ''}`}>
+                            <span className='name'>
+                                {m.id === id ? 'Вы' : m.userName}:
+                            </span>
+                            <span className='text'>{m.message}</span>
+                        </div>
+                    ))}
+                </div>
+                <form onSubmit={onSend}>
+                    <input
+                        type="text"
+                        value={text}
+                        onChange={event => setText(event.target.value)}
+                    />
+                </form>
+            </div>
+        </div>
     )
 };
 
--- server.js
+++ server.js
@@ -11,8 +11,11 @@ const webSocketServer = new WebSocket.Server({ server });
 
 const dispatchEvent = (message, ws) => {
     const json = JSON.parse(message);
+    console.log(json)
     switch (json.event) {
-        case "chat-message": webSocketServer.clients.forEach(client => client.send(message));
+        case "chat-message":
+            webSocketServer.clients.forEach(client => client.send(JSON.stringify(json.payload)));
+            break;
         default: ws.send((new Error("Wrong query")).message);
     }
 }
@@ -20,9 +23,7 @@ const dispatchEvent = (message, ws) => {
 webSocketServer.on('connection', ws => {
     ws.on('message', m => dispatchEvent(m, ws));
     ws.on("error", e => ws.send(e));
-
-    ws.send('Hi there, I am a WebSocket server');
 });
 
-server.listen(3000, () => console.log("Server started"))
+server.listen(5000, () => console.log("Server started"))
 
